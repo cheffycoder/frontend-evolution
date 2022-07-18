67315390e000ec42cd3ea3d5ffbc650056a03974
Shivam Bandral <shivambandral@gmail.com>
2022-07-19 03:24:53 +0530

    Added Babel to compile code to older JS

---
+++ es5/index.es5.js
@@ -0,0 +1,16 @@
+"use strict";
+
+var count = 0;
+
+function inc() {
+  count += 1;
+  document.getElementById("h1").textContent = count;
+} // Making button Element
+
+
+var $button = createElement('button', null, "Increment", {
+  click: function click() {
+    inc();
+  }
+});
+var $h1 = createElement('h1', 'h1', "0");
